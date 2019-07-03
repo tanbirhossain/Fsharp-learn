44a1822fdc95ae1cfd856b34b00900bdd7bab1c9
Mohammed Tanbir Hossain <pasic.tanbir@hotmail.com>
2019-07-03 13:27:10 +0900

    Inheritance

--- FsharpTest/Program.fs
+++ FsharpTest/Program.fs
@@ -648,3 +648,33 @@
 //printfn "%s" (c2.ToString())
 //printfn "%s" (c3.ToString())
 //printfn "%s" (c4.ToString())
+
+
+type Person(name) =
+    member x.Name = name
+    member x.Greet() = printfn "Hi, I'm %s" x.Name
+
+type Student(name,studentID :int) =
+    inherit Person(name)
+    let mutable _GPA = 0.0
+    member x.StudentID = studentID
+    member x.GPA
+        with get() = _GPA
+        and set value =_GPA <- value
+
+type Teacher(name, expertise:string) = 
+    inherit Person(name)
+    let mutable _salary = 0.0
+    member x.Salary
+        with get() = _salary
+        and set value = _salary <- value
+    member x.Expertise = expertise
+
+//using the subclasses
+let p = new Person("Mohan")
+let st = new Student("Zara",1234)
+let tr = new Teacher("Mariam", "Java")
+
+p.Greet()
+st.Greet()
+tr.Greet()
