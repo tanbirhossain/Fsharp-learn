f520d22d9243469aa364ba1f0f8dd7a46cec6741
Mohammed Tanbir Hossain <pasic.tanbir@hotmail.com>
2019-07-03 10:27:43 +0900

    struct

--- FsharpTest/Program.fs
+++ FsharpTest/Program.fs
@@ -590,3 +590,26 @@
 //gtr.PrintMessage()
 
 
+//type Line = struct
+//    val X1 : float
+//    val Y1: float
+//    val X2: float
+//    val Y2: float
+
+//    new (x1, y1 , x2,y2) =
+//        {
+//            X1 = x2; 
+//            X2 = x2;
+//            Y1 = y1;
+//            Y2 = y2;
+//        }
+
+//end
+
+//let calcLength(a : Line) =
+//    let sqr a = a*a
+//    sqrt(sqr(a.X1 - a.X2) + sqr(a.Y1 - a.Y2))
+
+//let aLine = new Line(1.0,1.0,4.0,5.0)
+//let length = calcLength aLine
+//printfn "Length of the Line: %g " length
