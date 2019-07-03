765ac7e33c9e3110b173daa0ff09154a0ef078c0
Mohammed Tanbir Hossain <pasic.tanbir@hotmail.com>
2019-07-03 13:40:41 +0900

    Abstract

--- FsharpTest/Program.fs
+++ FsharpTest/Program.fs
@@ -650,9 +650,41 @@
 //printfn "%s" (c4.ToString())
 
 
+//type Person(name) =
+//    member x.Name = name
+//    member x.Greet() = printfn "Hi, I'm %s" x.Name
+
+//type Student(name,studentID :int) =
+//    inherit Person(name)
+//    let mutable _GPA = 0.0
+//    member x.StudentID = studentID
+//    member x.GPA
+//        with get() = _GPA
+//        and set value =_GPA <- value
+
+//type Teacher(name, expertise:string) = 
+//    inherit Person(name)
+//    let mutable _salary = 0.0
+//    member x.Salary
+//        with get() = _salary
+//        and set value = _salary <- value
+//    member x.Expertise = expertise
+
+////using the subclasses
+//let p = new Person("Mohan")
+//let st = new Student("Zara",1234)
+//let tr = new Teacher("Mariam", "Java")
+
+//p.Greet()
+//st.Greet()
+//tr.Greet()
+
+
+//Abstract 
+[<AbstractClass>]
 type Person(name) =
     member x.Name = name
-    member x.Greet() = printfn "Hi, I'm %s" x.Name
+    abstract Greet : unit -> unit
 
 type Student(name,studentID :int) =
     inherit Person(name)
@@ -661,6 +693,7 @@ type Student(name,studentID :int) =
     member x.GPA
         with get() = _GPA
         and set value =_GPA <- value
+    override x.Greet() = printfn "Student %s" x.Name
 
 type Teacher(name, expertise:string) = 
     inherit Person(name)
@@ -669,12 +702,15 @@ type Teacher(name, expertise:string) =
         with get() = _salary
         and set value = _salary <- value
     member x.Expertise = expertise
+    override x.Greet() = printfn "Teacher %s." x.Name
+
+
 
 //using the subclasses
-let p = new Person("Mohan")
+
 let st = new Student("Zara",1234)
 let tr = new Teacher("Mariam", "Java")
 
-p.Greet()
+
 st.Greet()
 tr.Greet()
